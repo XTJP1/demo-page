da274d969996b8a8632e00772ebf8a42aeee1aca
Jacky <winterzaki@gmail.com>
2021-01-28 17:00:49 +0900

    Added an image

--- app.js
+++ app.js
@@ -22,6 +22,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+// show image
+app.use('/', express.static('resources'));
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
